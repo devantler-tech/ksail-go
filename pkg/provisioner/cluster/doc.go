7e6f86c4668901fb6107187bdecd6ac1aa9fd148
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-08-21 20:37:57 +0200

    refactor: move package documentation to appropriate files for better organization
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- pkg/provisioner/cluster/doc.go
+++
@@ -1,3 +0,0 @@
-// Package clusterprovisioner provides implementations of the Provisioner interface
-// for provisioning clusters in different providers.
-package clusterprovisioner
--- pkg/provisioner/cluster/provisioner.go
+++ pkg/provisioner/cluster/provisioner.go
@@ -1,3 +1,5 @@
+// Package clusterprovisioner provides implementations of the Provisioner interface
+// for provisioning clusters in different providers.
 package clusterprovisioner
 
 // ClusterProvisioner defines methods for managing Kubernetes clusters.
